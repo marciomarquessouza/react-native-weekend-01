dddc1d35a002147c5f1fb858405ae4ecde425556
Marcio Marques de Souza <marcio.souza@dextra-sw.com>
2019-09-28 19:22:48 -0300

    using reducer

--- src/screens/SquareScreen.js
+++ src/screens/SquareScreen.js
@@ -1,35 +1,52 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 import { StyleSheet, View } from 'react-native';
 import ColorToneSelector from '../components/ColorToneSelector';
 
+const COLOR_INCREMENT = 5;
+const reducer = (state, action) => {
+
+    switch (action.type) {
+        case "change_red":
+            return state.red > 255 || state.red < 0
+            ? state
+            : { ...state, red: state.red + action.payload };
+        case "change_green":
+            return state.green > 255 || state.green < 0
+            ? state
+            : { ...state, green: state.green + action.payload };
+        case "change_blue":
+            return state.blue > 255 || state.blue < 0
+            ? state
+            : { ...state, blue: state.blue + action.payload };
+        default:
+            return state;
+    }
+}
+
 const SquareScreen = () =>  {
 
-    const COLOR_INCREMENT = 5;
-    const [red, setRed] = useState(0);
-    const [green, setGreen] = useState(0);
-    const [blue, setBlue] = useState(0);
-    const increaseColor = (color) => ((color <= 255) ? (color + COLOR_INCREMENT) : 255);
-    const decreaseColor = (color) => ((color >= 0) ? (color - COLOR_INCREMENT) : 0);
+    const [state, dispatch] = useReducer(reducer, { red: 0, green: 0, blue: 0});
+    const { red, green, blue} = state;
 
     return (
         <View>
             <ColorToneSelector 
                 color="Red"
                 colorValue={ red }
-                handleIncrease = { () => setRed(increaseColor(red))}
-                handleDecrease = { () => setRed(decreaseColor(red))}
+                handleIncrease = { () => dispatch({ type: "change_red", payload: COLOR_INCREMENT }) }
+                handleDecrease = { () => dispatch({ type: "change_red", payload: COLOR_INCREMENT * -1 }) }
             />
             <ColorToneSelector 
                 color="Green"
                 colorValue={ green }
-                handleIncrease = { () => setGreen(increaseColor(green))}
-                handleDecrease = { () => setGreen(decreaseColor(green))}
+                handleIncrease = { () => dispatch({ type: "change_green", payload: COLOR_INCREMENT }) }
+                handleDecrease = { () => dispatch({ type: "change_green", payload: COLOR_INCREMENT * -1 }) }
             />
             <ColorToneSelector 
                 color="Blue"
                 colorValue={ blue }
-                handleIncrease = { () => setBlue(increaseColor(blue))}
-                handleDecrease = { () => setBlue(decreaseColor(blue))}
+                handleIncrease = { () => dispatch({ type: "change_blue", payload: COLOR_INCREMENT }) }
+                handleDecrease = { () => dispatch({ type: "change_blue", payload: COLOR_INCREMENT * -1 }) }
             />
             <View
                 style = {{
